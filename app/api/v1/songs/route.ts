949856d5ada06e317932616e99b893192ae487ed
Your Name <you@example.com>
2024-11-15 13:57:00 -0600

    remove Swagger

--- app/api/v1/songs/route.ts
+++ app/api/v1/songs/route.ts
@@ -2,35 +2,6 @@ import { songs } from "@/data/songs/songs";
 
 export const dynamic = "force-static";
 
-/**
- * @swagger
- * /api/v1/songs:
- *   get:
- *     description: Returns list of all songs
- *     tags:
- *      - Songs
- *     responses:
- *       200:
- *         description: Songs array
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   id:
- *                     type: string
- *                   title:
- *                     type: string
- *                   artist:
- *                     type: string
- *                   genre:
- *                     type: string
- *                   duration:
- *                     type: number
- */
-
 export const GET = () => {
   return Response.json(
     songs.map((song) => ({
